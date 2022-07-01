4d8445f71f49a5fef27fd8c88d4ce77bf56b5b40
Demian Pashkovskyi <skyangely4@gmail.com>
2022-07-01 15:20:43 +0300

    js-hw-11 v.2.0

--- src/index.js
+++ src/index.js
@@ -1,42 +1,113 @@
+import './sass/main.scss';
+'use strict';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from "axios";
-const API_KEY = '28320130-19a63cf24e9bf5fe751a1157e';
+import ImagesAPIService from './js/fetch-img';
+import LoadMoreBtn from './js/load-more-btn';
+import Markup from './js/imagemerkup';
 
 const refs = {
-	form: document.querySelector('#search-form'),
-	gallery: document.querySelector('.gallery'),
-	loadMoreBtn: document.querySelector('.load-more'),
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
-refs.form.addEventListener("submit", onFormSubmit);
+const imagesAPIService = new ImagesAPIService();
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more' });
+const renderMarkup = new Markup({ selector: refs.gallery });
 
-function onFormSubmit(event) {
-	event.preventDefault();
-	refs.gallery.innerHTML = "";
-	imagesAPIService.query = event.currentTarget.searchQuery.value.trim();
+refs.form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.button.addEventListener('click', onloadMoreBtnClick);
 
-	if (imagesAPIService.query === "") {
-		Notify.info('Your query is empty. Try again!');
-		return;
-	} 
+// Submit handler
+async function onFormSubmit(event) {
+  event.preventDefault();
+  renderMarkup.reset();
+  imagesAPIService.query = event.currentTarget.searchQuery.value.trim();
+
+  if (imagesAPIService.query === '') {
+    loadMoreBtn.hideBtn();
+    Notify.info('Your query is empty. Try again!');
+    return;
+  }
+
+  imagesAPIService.resetPage();
+
+  try {
+    loadMoreBtn.showBtn();
+    await initFetchImages();
+  } catch (error) {
+    loadMoreBtn.hideBtn();
+    Notify.failure(error.message);
+  }
+
+  refs.form.reset();
 }
 
-function fetchImages (imagesName) {
-	const options = new URLSearchParams({
-		key: `${API_KEY}`,
-      q: `${this.searchQuery}`,
-      page: `${this.page}`,
-      per_page: `${this.PER_PAGE}`,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-	});
+// Load-More Button handler
+async function onloadMoreBtnClick() {
+	try {
+	  await initFetchImages();
+	} catch {
+	  Notify.failure(error.message);
+	}
+	// pageScroll();
+	renderMarkup.lightbox.refresh();
+ }
+ 
+ // Send request
+ async function initFetchImages() {
+	
+	try {
+	  loadMoreBtn.disable();
+	  const images = await imagesAPIService.fetchImages();
+	  renderMarkup.items = images;
+	  renderMarkup.render();
+	} catch {
+	  Notify.failure(error.message);
+	}
+	 
+ 
+	if (imagesAPIService.endOfHits) {
+	  loadMoreBtn.hideBtn();
+	  return;
+	}
+	loadMoreBtn.enable();
+ }
+ 
+// scroll to top
+const toTopBtn = document.getElementById('topBTN');
 
-	return fetch(`https://pixabay.com/api/?${options}`).then(res => {
-		if (res.ok) {
-			return res.json()
-		}
-		throw new Error(res.statusText);
-	});
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopBtn.style.display = 'block';
+    toTopBtn.addEventListener('click', topFunction);
+  } else {
+    toTopBtn.style.display = 'none';
+  }
 }
-fetchImages("cat").then(data => console.log(data));
+
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+// infinite scroll
+const options = {
+	rootMargin: '200px',
+	threshold: 1.0,
+ };
+ 
+ const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+	  if (entry.isIntersecting && imagesAPIService.query ) {
+		onloadMoreBtnClick();
+	  }
+	});
+ }, options);
+ 
+ observer.observe(document.querySelector('.scroll-guard'));
--- src/js/fetch-img.js
+++ src/js/fetch-img.js
@@ -1,31 +1,87 @@
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from "axios";
 
-var API_KEY = '28320130-19a63cf24e9bf5fe751a1157e';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-   $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-   console.log('No hits');
-});
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '28320130-19a63cf24e9bf5fe751a1157e';
 
-function fetchImages (imagesName) {
-	const options = new URLSearchParams({
-		key: `${API_KEY}`,
+export default class ImagesAPIService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+    this.totalHits = null;
+    this.totalPages = null;
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  getOptions() {
+    const options = new URLSearchParams({
+      key: `${API_KEY}`,
       q: `${this.searchQuery}`,
       page: `${this.page}`,
       per_page: `${this.PER_PAGE}`,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-	});
+    });
+    return options;
+  }
+  incrementPage() {
+    this.page += 1;
+  }
 
-	return fetch(`https://pixabay.com/api/?${options}`).then(res => {
-		if (res.ok) {
-			return res.json()
-		}
-		throw new Error(res.statusText);
-	});
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  async fetchImages() {
+    try {
+      const options = this.getOptions();
+      const response = await axios.get(`?${options}`);
+      const data = await response.data;
+     
+      this.totalHits = data.totalHits;
+      this.totalPages = Math.ceil(this.totalHits / this.PER_PAGE);
+      this.resetEndOfHits();
+    
+        if (data.total === 0) {
+        throw new Error('Sorry, there are no images matching your search query. Please try again.');
+      }
+       
+      // try {
+        const images = await data.hits;
+        this.notificationOnFirstPage();
+        this.notificationForEndHits();
+        this.incrementPage();
+        return images;
+    } catch {
+      Notify.failure(error.message);
+     } 
+  }
+
+  
+  notificationOnFirstPage() {
+    if (this.page === 1) {
+      Notify.success(`Hooray! We found ${this.totalHits} images.`);
+    }
+  }
+
+  notificationForEndHits() {
+    if (this.page === this.totalPages) {
+      this.endOfHits = true;
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  }
 }
-fetchImages("dog").then(data => console.log(data));
